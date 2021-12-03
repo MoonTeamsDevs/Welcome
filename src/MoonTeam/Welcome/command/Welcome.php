8c0c18ecf73f1847875d29b32fda84906efe976f
MoonTeam <94476384+MoonTeamsDevs@users.noreply.github.com>
2021-12-03 19:15:56 +0100

    update

--- src/MoonTeam/Welcome/command/Welcome.php
+++ src/MoonTeam/Welcome/command/Welcome.php
@@ -3,7 +3,7 @@
 namespace MoonTeam\Welcome\command;
 
 use MoonTeam\Welcome\listener\PlayerListener;
-use MoonTeams\Welcome\Main;
+use MoonTeam\Welcome\Main;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\console\ConsoleCommandSender;
@@ -20,29 +20,9 @@ class Welcome extends Command {
 
     public function execute(CommandSender $sender, string $commandLabel, array $args)
     {
-        if ($sender instanceof Player){
-            if (!isset(Main::$cooldown[$sender->getName()])) {
-                if (empty(PlayerListener::$welcome)) {
-                    $sender->sendMessage(Main::getInstance()->getConfig()->get("prefix") . Main::getInstance()->getConfig()->get("no-new-players"));
-                    return;
-                } else {
-                    $res = [];
-                    foreach (PlayerListener::$welcome as $player => $time) {
-                        $res[] = $player;
-                    }
-                    $players = implode(", ", $res);
-                    Server::getInstance()->broadcastMessage(str_replace(["{player}", "{players}"], [$sender->getName(), $players], Main::getInstance()->getConfig()->get("welcome-player-message")));
-                    Main::$cooldown[$sender->getName()] = time() + Main::getInstance()->getConfig()->get("cooldown");
-                    if (Main::getInstance()->getConfig()->get("enable-command")){
-                        if (!empty(Main::getInstance()->getConfig()->get("commands"))) {
-                            foreach (Main::getInstance()->getConfig()->get("commands") as $command) {
-                                Server::getInstance()->dispatchCommand(new ConsoleCommandSender(Server::getInstance(), Server::getInstance()->getLanguage()), str_replace(["{player}"], [$sender->getName()], $command));
-                            }
-                        }
-                    }
-                }
-            } else {
-                if (time() >= Main::$cooldown[$sender->getName()]) {
+        if ($sender instanceof Player) {
+            if ($sender->hasPermission("welcome")) {
+                if (!isset(Main::$cooldown[$sender->getName()])) {
                     if (empty(PlayerListener::$welcome)) {
                         $sender->sendMessage(Main::getInstance()->getConfig()->get("prefix") . Main::getInstance()->getConfig()->get("no-new-players"));
                         return;
@@ -54,7 +34,7 @@ class Welcome extends Command {
                         $players = implode(", ", $res);
                         Server::getInstance()->broadcastMessage(str_replace(["{player}", "{players}"], [$sender->getName(), $players], Main::getInstance()->getConfig()->get("welcome-player-message")));
                         Main::$cooldown[$sender->getName()] = time() + Main::getInstance()->getConfig()->get("cooldown");
-                        if (Main::getInstance()->getConfig()->get("enable-command")){
+                        if (Main::getInstance()->getConfig()->get("enable-command")) {
                             if (!empty(Main::getInstance()->getConfig()->get("commands"))) {
                                 foreach (Main::getInstance()->getConfig()->get("commands") as $command) {
                                     Server::getInstance()->dispatchCommand(new ConsoleCommandSender(Server::getInstance(), Server::getInstance()->getLanguage()), str_replace(["{player}"], [$sender->getName()], $command));
@@ -63,8 +43,30 @@ class Welcome extends Command {
                         }
                     }
                 } else {
-                    $sender->sendMessage(Main::getInstance()->getConfig()->get("prefix") . str_replace(["{time}"], [Main::$cooldown[$sender->getName()] - time()], Main::getInstance()->getConfig()->get("cooldown-message")));
-                    return;
+                    if (time() >= Main::$cooldown[$sender->getName()]) {
+                        if (empty(PlayerListener::$welcome)) {
+                            $sender->sendMessage(Main::getInstance()->getConfig()->get("prefix") . Main::getInstance()->getConfig()->get("no-new-players"));
+                            return;
+                        } else {
+                            $res = [];
+                            foreach (PlayerListener::$welcome as $player => $time) {
+                                $res[] = $player;
+                            }
+                            $players = implode(", ", $res);
+                            Server::getInstance()->broadcastMessage(str_replace(["{player}", "{players}"], [$sender->getName(), $players], Main::getInstance()->getConfig()->get("welcome-player-message")));
+                            Main::$cooldown[$sender->getName()] = time() + Main::getInstance()->getConfig()->get("cooldown");
+                            if (Main::getInstance()->getConfig()->get("enable-command")) {
+                                if (!empty(Main::getInstance()->getConfig()->get("commands"))) {
+                                    foreach (Main::getInstance()->getConfig()->get("commands") as $command) {
+                                        Server::getInstance()->dispatchCommand(new ConsoleCommandSender(Server::getInstance(), Server::getInstance()->getLanguage()), str_replace(["{player}"], [$sender->getName()], $command));
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        $sender->sendMessage(Main::getInstance()->getConfig()->get("prefix") . str_replace(["{time}"], [Main::$cooldown[$sender->getName()] - time()], Main::getInstance()->getConfig()->get("cooldown-message")));
+                        return;
+                    }
                 }
             }
         }
